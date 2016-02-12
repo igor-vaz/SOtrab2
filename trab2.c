2242e0d4fb6ee549ecdbff0670f835fa72317bed
igor-vaz <ivaz567@hotmail.com>
2016-02-12 18:14:51 -0200

    criando threads a cada 3s

--- trab2.c
+++ trab2.c
@@ -13,9 +13,15 @@ void *aviso(void * id){
 	printf("thread %d criada\n", tid);
 }
 
+void esperaPor (unsigned int segs) {
+    unsigned int tempoRet = time(0) + segs;
+    while (time(0) < tempoRet);
+}
+
 int main(int argc, char const *argv[]){
 
 	int nThreads,nPaginas,t;
+	unsigned int secs;
 
 	if (argc < 3){
 		printf("rodar com %s <#threads> <#paginas>\n", argv[0]);
@@ -26,17 +32,19 @@ int main(int argc, char const *argv[]){
 	printf("%d\n", nThreads);
 	pthread_t tid[nThreads];
 
-	for(t=0; t<nThreads; t++) {
+	for(t=0; t<nThreads; t++){
+		esperaPor(3);
+		printf("criando thread no segundo %d \n", (t+1)*3);
 		int *id = malloc(sizeof(int));
 		*id = t;
 		//cria threads
-		if (pthread_create(&tid[t], NULL, aviso, (void *)id)) {
+		if (pthread_create(&tid[t], NULL, aviso, (void *)id)){
 		  printf("--ERRO: pthread_create()\n"); exit(-1);
 		}
 	}
 	//espera todas as threads terminarem
-	for (t=0; t<nThreads; t++) {
-    	if (pthread_join(tid[t], NULL)) {
+	for (t=0; t<nThreads; t++){
+    	if (pthread_join(tid[t], NULL)){
         	printf("--ERRO: pthread_join() \n"); exit(-1); 
     	} 
   	} 
